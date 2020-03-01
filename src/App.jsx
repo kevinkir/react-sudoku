368700a3316a97228024579f91a95428483db05b
Kevin Kirchhoff <kevin.kirchhoff@logmein.com>
2020-03-01 12:25:42 -0800

    add hot module replacement

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
+
 import './App.css';
 
-export function App() {
+export const App = hot(module)(() => {
   return (
     <div className='App'>
       <h1> Hello, World! </h1>
     </div>
   );
-}
+});
