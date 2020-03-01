79e5508c31e5ef432c0e11e3e74350706c4ab2b0
Kevin Kirchhoff <kevin.kirchhoff@logmein.com>
2020-03-01 12:47:32 -0800

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 
 import { App } from './components/App';
-import rootReducer from './reducers';
+import { configureStore } from './store';
 
-const store = createStore(rootReducer);
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
