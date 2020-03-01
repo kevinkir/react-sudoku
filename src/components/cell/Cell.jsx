e9ee13a7d5baf9ba2fcc0d2a5abbf030a8cb1b04
Kevin Kirchhoff <kevin.kirchhoff@logmein.com>
2020-03-01 13:17:58 -0800

    add cell component

---
+++ src/components/cell/Cell.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Cell = ({ onChange, value }) => (
+  <div className="cell-container">
+    <input
+      type="number"
+      className="cell-input"
+      onChange={(event) => {
+        onChange(event.target.value);
+      }}
+      value={value}
+    />
+  </div>
+);
+
+Cell.propTypes = {
+  onChange: PropTypes.func,
+};
